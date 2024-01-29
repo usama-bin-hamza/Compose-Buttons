e514f247c7fb44bfdffe95e786e57a63b7b208cf
Usama Bin Hamza <96182703+usama-bin-hamza@users.noreply.github.com>
2024-01-29 20:03:28 +0500

    Full Project

--- app/src/main/java/com/usama/composebuttons/MainActivity.kt
+++ app/src/main/java/com/usama/composebuttons/MainActivity.kt
@@ -3,13 +3,31 @@ package com.usama.composebuttons
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.usama.composebuttons.ui.theme.ComposeButtonsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +35,49 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeButtonsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+               Surface(
+                   modifier = Modifier
+                       .fillMaxSize()
+                       .background(color = MaterialTheme.colorScheme.background),
+               ) {
+                   Column(
+                       modifier = Modifier
+                           .fillMaxSize(),
+                       verticalArrangement = Arrangement.Center,
+                       horizontalAlignment = Alignment.CenterHorizontally
+                   ) {
+                       Button(onClick = { /*TODO*/ }) {
+                           Text(text = "Subscribe")
+                       }
+
+                       Spacer(modifier = Modifier.padding(top = 10.dp))
+
+                       ElevatedButton(onClick = { /*TODO*/ }) {
+                           Icon(
+                               imageVector = Icons.Default.Add,
+                               contentDescription = "Add to cart",
+                               modifier = Modifier
+//                                   .padding(end = 8.dp)
+                                   .size(18.dp)
+                           )
+                           Spacer(modifier = Modifier.width(8.dp))
+                           Text(text = "Add to cart")
+                       }
+
+                       FilledTonalButton(onClick = { /*TODO*/ }) {
+                           Text(text = "Open in Browser")
+                       }
+
+                       OutlinedButton(onClick = { /*TODO*/ }) {
+                           Text(text = "Back")
+                       }
+                       
+                       TextButton(onClick = { /*TODO*/ }) {
+                           Text(text = "Learn More")
+                       }
+                   }
+               }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeButtonsTheme {
-        Greeting("Android")
     }
 }
